e4b6e72e9490737f5a932992d2e9ccb3e7eb6789
shunsuke-ue <shunsuke.ue@gmail.com>
2023-06-25 08:17:35 +0000

    add Devise gem

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  # リクエストしてきたユーザーを認証する。
+  # ユーザーがログイン済みの場合はアクセスを許可して、未ログインの場合はroot_pathにリダイレクトする。
+  before_action :authenticate_user!
+
+  def index
+  end
+end
